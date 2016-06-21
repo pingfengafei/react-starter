59e88f2c422ecb5f93fb483089b50d3ebe113454
zadzbw <609842385@qq.com>
2016-06-21 19:36:32 +0800

    update react-router demo

--- app/components/Boys/Boys.jsx
+++ app/components/Boys/Boys.jsx
@@ -5,6 +5,15 @@ export default class Boys extends React.Component {
 
     constructor(props) {
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const boyName = event.target.elements[0].value;
+        const path = `/boys/${boyName}`;
+        console.log(path);
+        this.context.router.push(path);
     }
 
     render() {
@@ -14,9 +23,19 @@ export default class Boys extends React.Component {
                 <ul>
                     <li><Link to="/boys/宋仲基">宋仲基</Link></li>
                     <li><Link to="/boys/吴亦凡">吴亦凡</Link></li>
+                    <li>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" placeholder="boyName"/> / {' '}
+                            <button type="submit">Go</button>
+                        </form>
+                    </li>
                 </ul>
                 {this.props.children}
             </div>
         );
     }
 }
+
+Boys.contextTypes = {
+    router: React.PropTypes.object
+};
