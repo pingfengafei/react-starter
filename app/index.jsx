9bf016b7f82beb8291dddfa3a5d880deb5a560e8
zadzbw <609842385@qq.com>
2016-06-21 14:22:31 +0800

    lodash试用,以及监听事件

--- app/index.jsx
+++ app/index.jsx
@@ -6,9 +6,31 @@ import FirstApp from './components/FirstApp/FirstApp';
 import HelloWorld from './components/HelloWorld/HelloWorld';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {w: window.innerWidth};
+        this.handleResize = this.handleResize.bind(this);
+    }
+
+    handleResize() {
+        this.setState({
+            w: window.innerWidth
+        });
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', _.throttle(this.handleResize, 600, {'trailing': false}));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', _.throttle(this.handleResize, 600, {'trailing': false}));
+    }
+
     render() {
         return (
             <div>
+                <div>width:{this.state.w}</div>
                 <FirstApp />
                 <HelloWorld />
             </div>
