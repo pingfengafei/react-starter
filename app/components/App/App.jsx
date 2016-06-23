adca731911348fb853fba37c3cac0fa7ddbf94d3
zadzbw <609842385@qq.com>
2016-06-23 18:18:17 +0800

    测试promise对象

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -24,6 +24,22 @@ export default class App extends React.Component {
             console.log(error);
         });
 
+        var p1 = new Promise(function (resolve) {
+            setTimeout(function () {
+                resolve("Hello");
+            }, 3000);
+        });
+
+        var p2 = new Promise(function (resolve) {
+            setTimeout(function () {
+                resolve("World");
+            }, 1000);
+        });
+
+        Promise.all([p1, p2]).then(function (result) {
+            console.log(result); // ["Hello", "World"]
+        });
+
         return (
             <div>
                 <h1>Ghettohub Issues</h1>
