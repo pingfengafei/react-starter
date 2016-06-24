37b19cd25e6988df8bfe7181bfcdce7e50cbd5ff
zadzbw <609842385@qq.com>
2016-06-24 18:31:59 +0800

    去除promise的测试代码

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -9,37 +9,6 @@ export default class App extends React.Component {
 
     render() {
 
-        let promise = new Promise(function (resolve, reject) {
-            var a = Math.random();
-            if (a > 0.5) {
-                resolve('go');
-            } else {
-                reject('error');
-            }
-        });
-
-        promise.then((value)=> {
-            console.log(value);
-        }, (error)=> {
-            console.log(error);
-        });
-
-        var p1 = new Promise(function (resolve) {
-            setTimeout(function () {
-                resolve('Hello');
-            }, 3000);
-        });
-
-        var p2 = new Promise(function (resolve) {
-            setTimeout(function () {
-                resolve('World');
-            }, 1000);
-        });
-
-        Promise.all([p1, p2]).then(function (result) {
-            console.log(result); // ["Hello", "World"]
-        });
-
         return (
             <div>
                 <h1>Ghettohub Issues</h1>
