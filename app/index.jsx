d1d1c90100ae7a5a690bfebaefa233aa67a7f910
zadzbw <609842385@qq.com>
2016-07-12 17:15:22 +0800

    增加redux的demo

--- app/index.jsx
+++ app/index.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 import MyButtonController from './components/MyButton/MyButtonController';
-import App from './components/App/App';
+import Demo from './containers/Demo';
 import Counter from './components/Counter/Counter';
 
 ReactDOM.render(
     <Router history={browserHistory}>
-        <Route path="/" component={App}>
+        <Route path="/" component={Demo}>
             <Route path="/demo" component={MyButtonController}/>
             <Route path="/count" component={Counter}/>
         </Route>
