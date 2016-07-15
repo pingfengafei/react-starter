f892363e753acdce58b5f91e2d56dc730ece51d5
zadzbw <609842385@qq.com>
2016-07-15 15:12:02 +0800

    增加异步action

--- app/reducers/asyncReducer.js
+++ app/reducers/asyncReducer.js
@@ -5,11 +5,11 @@ import {AUTHORIZE, AUTHORIZE_SUCCESS, AUTHORIZE_FAIL} from '../actions/AsyncActi
 export default function (state = {text: {}, status: 'wait'}, action) {
     switch (action.type) {
         case AUTHORIZE:
-            return {text: {name: action.username, pwd: action.password}, status: 'onprogress'};
+            return {text: action.data, status: 'onprogress'};
         case AUTHORIZE_SUCCESS:
-            return {text: action.response, status: 'success'};
+            return {text: action.data, status: 'success'};
         case AUTHORIZE_FAIL:
-            return {text: action.error, status: 'fail'};
+            return {text: action.data, status: 'fail'};
         default:
             return state;
     }
